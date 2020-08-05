ad2c2a24bac04690ebd11715b1a4654b3b84f44c
Thitipon Tienyam <thitipontie@mathregit.group>
2020-08-05 13:26:45 +0700

    add rq param

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,16 +1,31 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
 
+//    @GetMapping("/users")
+//    public List<UsersResponse> getAllUser(@RequestParam(required = false) int page, int itemPerPage){
+//        List<UsersResponse> usersResponseList = new ArrayList<>();
+//        usersResponseList.add(new UsersResponse(1, "user1"));
+//        usersResponseList.add(new UsersResponse(2, "user2"));
+//        usersResponseList.add(new UsersResponse(3, "user3"));
+//        return usersResponseList;
+//    }
+
     @GetMapping("/users")
-    public UsersResponse[] getAllUser(){
-        UsersResponse[] usersResponses = new UsersResponse[2];
-        usersResponses[0] = new UsersResponse(1, "user1");
-        usersResponses[1] = new UsersResponse(1, "user2");
-        return usersResponses;
+    public String getPage(@RequestParam(required = false,defaultValue = "1") String page, @RequestParam(name = "item_per_page",required = false,defaultValue = "15") String itemPerPage) {
+        return "Page: " + page + " , itemPerPage: " + itemPerPage;
+
+    }
+
+    @GetMapping("/users/{id}")
+    @ResponseBody
+    public UsersResponse getUserById(@PathVariable int id){
+        return new UsersResponse(id, "User " + id);
     }
 }
