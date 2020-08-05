226a55413c255033d1e38b8f0edbd38e7033e935
Thitipon Tienyam <thitipontie@mathregit.group>
2020-08-05 14:33:57 +0700

    Post

---
+++ src/main/java/com/example/demo/PagingResponse.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.List;
+
+@JsonPropertyOrder({"page", "item_per_page", "users"})
+
+public class PagingResponse {
+    @JsonProperty("users")
+    private List<UsersResponse> usersResponseList;
+
+    private int page;
+
+    @JsonProperty("item_per_page")
+    private int itemPerPage;
+
+    public PagingResponse(int page, int itemPerPage) {
+        this.page = page;
+        this.itemPerPage = itemPerPage;
+    }
+
+    public void setUsersResponse(List<UsersResponse> usersResponseList) {
+        this.usersResponseList = usersResponseList;
+    }
+
+    public List<UsersResponse> getUsersResponseList() {
+        return usersResponseList;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getItemPerPage() {
+        return itemPerPage;
+    }
+
+    public void setItemPerPage(int itemPerPage) {
+        this.itemPerPage = itemPerPage;
+    }
+}
--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -8,24 +8,36 @@ import java.util.List;
 @RestController
 public class UserController {
 
-//    @GetMapping("/users")
-//    public List<UsersResponse> getAllUser(@RequestParam(required = false) int page, int itemPerPage){
-//        List<UsersResponse> usersResponseList = new ArrayList<>();
-//        usersResponseList.add(new UsersResponse(1, "user1"));
-//        usersResponseList.add(new UsersResponse(2, "user2"));
-//        usersResponseList.add(new UsersResponse(3, "user3"));
-//        return usersResponseList;
-//    }
-
     @GetMapping("/users")
-    public String getPage(@RequestParam(required = false,defaultValue = "1") String page, @RequestParam(name = "item_per_page",required = false,defaultValue = "15") String itemPerPage) {
-        return "Page: " + page + " , itemPerPage: " + itemPerPage;
+    public PagingResponse getAllUser(
+             @RequestParam(defaultValue = "1") int page,
+             @RequestParam(name = "item_per_page" ,defaultValue = "10") int itemPerPage){
 
+        PagingResponse pagingResponse = new PagingResponse(page, itemPerPage);
+        List<UsersResponse> usersResponseList = new ArrayList<>();
+        usersResponseList.add(new UsersResponse(1, "user1"));
+        usersResponseList.add(new UsersResponse(2, "user2"));
+        usersResponseList.add(new UsersResponse(3, "user3"));
+
+        pagingResponse.setUsersResponse(usersResponseList);
+        return pagingResponse;
     }
 
+
+
+//    @GetMapping("/users")
+//    public String getPage(@RequestParam(required = false,defaultValue = "1") String page, @RequestParam(name = "item_per_page",required = false,defaultValue = "15") String itemPerPage) {
+//        return "Page: " + page + " , itemPerPage: " + itemPerPage;
+//
+//    }
+
     @GetMapping("/users/{id}")
-    @ResponseBody
     public UsersResponse getUserById(@PathVariable int id){
         return new UsersResponse(id, "User " + id);
+    }
+
+    @PostMapping("/users")
+    public UsersResponse createNewUser(@RequestBody NewUserRequest request){
+        return new UsersResponse(0,request.getName() + request.getAge());
     }
 }
